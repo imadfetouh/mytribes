734d4ea53598fec81328143bb8da8a18692c4172
Imad el Fetouh <imad.elfetouh@hotmaill.com>
2020-04-15 09:28:58 +0200

    port changed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var PORT = process.env.PORT || 3000;
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
@@ -31,6 +32,6 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(PORT, function(){
+    console.log('listening on port %d', PORT);
 });
